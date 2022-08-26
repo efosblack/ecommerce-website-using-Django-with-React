b35557187e779e547972cf40e438916a7c5f644a
efosblack <eriamiatoeefe11@gmail.com>
2022-08-26 18:28:39 -0400

    linking frontend to backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 function ProductScreen() {
+
+    const [product, setProduct] = useState([])
     const { id } = useParams();
-    const product = products.find((p) => p._id === (id));
+
+    useEffect(() =>{
+        async function fetchProduct() {
+            const { data } =  await axios.get(`/api/products/${id}`)
+            setProduct(data)
+        }
+        fetchProduct()
+
+    },[])
+
+
     return (
       <div>
         <Link to='/' className='btn btn-light my-3'>Go Back</Link>
@@ -23,7 +35,7 @@ function ProductScreen() {
                     </ListGroup.Item>
 
                     <ListGroup.Item>
-                        <Rating value={product.name} text={`${product.numReviews} reviews`} color={'#f8e825'}/>
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'}/>
                     </ListGroup.Item>
 
                     <ListGroup.Item>
